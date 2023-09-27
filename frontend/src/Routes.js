b5013ddaf8d3e9fe32a8b7d56e6a55fb9c2b788e
Emanuel Garcia <wemagw@gmail.com>
2023-09-27 12:34:34 -0300

    Sign Up DONE

--- frontend/src/Routes.js
+++ frontend/src/Routes.js
@@ -1,6 +1,8 @@
-import LoginPage from "../src/pages/Login.jsx";
+import LoginPage from "../src/pages/Login";
+import SignupPage from "../src/pages/Signup";
 
 export {
     LoginPage,
+    SignupPage,
 }
 
