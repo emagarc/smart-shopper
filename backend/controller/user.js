e19b592c60f651d0b730282a4020ee811d409ece
Emanuel Garcia <wemagw@gmail.com>
2023-09-28 12:19:18 -0300

    User controller

--- backend/controller/user.js
+++ backend/controller/user.js
@@ -4,26 +4,57 @@ const User = require("../model/user");
 const router = express.Router();
 const { upload } = require("../multer");
 const ErrorHandler = require("../utils/error-handler");
+const fs = require("fs");
 
 router.post("/create-user", upload.single("file"), async (req, res, next) => {
-    const {name, email, password} = req.body;
-    const userEmail = await User.findOne({email});
+  const { name, email, password } = req.body;
+
+  try {
+    const userEmail = await User.findOne({ email });
 
     if (userEmail) {
-        return next(new ErrorHandler("User already exists", 400));
+      // Usuario ya existe, lanzar el error
+      throw new ErrorHandler("User already exists", 400);
     }
 
-    const fileName = req.file.filename;
-    const fileUrl = path.join(fileName);
-
     const user = {
-        name: name,
-        email: email,
-        password: password,
-        avatar: fileUrl
+      name: name,
+      email: email,
+      password: password,
+      avatar: {
+        public_id: req.file.filename,
+        url: req.file.path,
+      },
     };
 
-    console.log(user);
+    const newUser = await User.create(user);
+
+    res.status(201).json({
+      success: true,
+      newUser,
+    });
+  } catch (error) {
+    // Lanzar el error
+    console.error("Error creating user:", error);
+
+    // Eliminar la imagen si la carga falla
+    const filename = req.file.filename;
+    const filePath = `uploads/${filename}`;
+
+    console.log("Attempting to delete file at path:", filePath);
+
+    try {
+      // Intentar eliminar el archivo
+      await fs.promises.unlink(filePath);
+      console.log("File deleted successfully");
+    } catch (deleteError) {
+      // Manejar el error si no se puede eliminar el archivo
+      console.error("Error deleting file:", deleteError);
+    }
+
+    // Lanzar el error después de intentar eliminar el archivo
+    next(error);
+  }
 });
 
 module.exports = router;
--- frontend/src/components/Signup/Signup.jsx
+++ frontend/src/components/Signup/Signup.jsx
@@ -1,4 +1,6 @@
 import { React, useState } from "react";
+import server from "../../server";
+import axios from "axios";
 import { AiOutlineEye, AiOutlineEyeInvisible } from "react-icons/ai";
 import styles from "../../styles/styles";
 import { Link } from "react-router-dom";
@@ -12,14 +14,31 @@ const Signup = () => {
   const [visible, setVisible] = useState(false);
   const [avatar, setAvatar] = useState(null);
 
-  const handleSubmit = () => {
-    console.log("HandleSubmit");
-  };
-
   const handleFileInputChange = (e) => {
     const file = e.target.files[0];
     setAvatar(file);
   };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const config = { headers: { "Content-Type": "multipart/form-data" } };
+    const newForm = new FormData();
+
+    newForm.append("file", avatar);
+    newForm.append("name", name);
+    newForm.append("email", email);
+    newForm.append("password", password);
+
+    axios
+      .post(`${server}/user/create-user`, newForm, config)
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
   return (
     <div className="min-h-screen bg-gray-50 flex flex-col justify-center py-12 sm:px-6 lg:px-8">
       <div className="sm:mx-auto sm:w-full sm:max-w-md">
@@ -29,7 +48,7 @@ const Signup = () => {
       </div>
       <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
         <div className="bg-white py-8 px-4 shadow sm:rounded-lg sm:px-10">
-          <form className="space-y-6">
+          <form className="space-y-6" onSubmit={handleSubmit}>
             <div>
               <label
                 htmlFor="email"
